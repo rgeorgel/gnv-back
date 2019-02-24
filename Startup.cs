1c1074cca0d17ba12b808e7977bc1d5714352d75
Ricardo Georgel <rickygeorgel@yahoo.com.br>
2019-02-24 11:07:06 -0300

    add cors

--- Startup.cs
+++ Startup.cs
@@ -62,6 +62,12 @@ namespace gnv_back
                 }
             }
 
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowOrigin",
+                    builder => builder.AllowAnyOrigin());
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             services.AddScoped<IStationBusiness, StationBusinessImpl>();
@@ -82,6 +88,8 @@ namespace gnv_back
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseCors("AllowOrigin");
 
             app.UseHttpsRedirection();
             app.UseMvc();
