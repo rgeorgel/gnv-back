0cc43caa608b68649be047858f32723791edee0a
Ricardo Georgel <rickygeorgel@yahoo.com.br>
2019-08-18 21:26:46 -0300

    fix on steps order of publish

--- publish.sh
+++ publish.sh
@@ -9,11 +9,11 @@
 
 #Login no heroku
 heroku container:login
-#remove container atual
-heroku container:rm web -a gnv-no-app
 #build da imagem
 docker build -t gnv-no-app .
 docker tag gnv-no-app registry.heroku.com/gnv-no-app/web
 docker push registry.heroku.com/gnv-no-app/web
+#remove container atual
+heroku container:rm web -a gnv-no-app
 #publicação da imagem no heroku
 heroku container:release web -a gnv-no-app
